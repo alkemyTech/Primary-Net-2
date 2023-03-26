6800d41c95bea16112bfd3111877e2b1ee82df0b
Gonzalo <102692082+gnz6@users.noreply.github.com>
2023-03-26 17:48:07 -0300

    Desicion de grupo: cambio de nombre entidad catalogues a FixedTermDeposit

---
+++ PrimatesWallet.Core/Models/FixedTermDeposit.cs
@@ -0,0 +1,29 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PrimatesWallet.Core.Models
+public class FixedTermDeposit
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Required]
+    [Column("user_id")]
+    public int UserId { get; set; }
+
+    [ForeignKey("UserId")]
+    public User User { get; set; }
+
+    [Required]
+    [Column("account_id")]
+    public int AccountId { get; set; }
+
+    [ForeignKey("AccountId")]
+    public Account Account { get; set; }
+
+    [Required]
+    [Column("amount", TypeName = "decimal(18,2)")]
+    public decimal Amount { get; set; }
+}
