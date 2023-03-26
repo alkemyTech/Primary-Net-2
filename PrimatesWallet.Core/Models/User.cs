287e1f583dab05c811b7f01eef30bfeb21898975
Luciano Escobar <luciano.esc@hotmail.com>
2023-03-25 21:19:37 -0300

    Modelo User

---
+++ PrimatesWallet.Core/Models/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PrimatesWallet.Core.Models
+{
+    [Index(nameof(Email), IsUnique = true)]
+    public class User
+    {
+        [Key]
+        [Column("user_id")]
+        public int UserId { get; set; }
+
+        [Column("first_name", TypeName = "VARCHAR")]
+        public string First_Name { get; set; }
+
+        [Column("last_name", TypeName = "VARCHAR")]
+        public string Last_Name { get; set; }
+
+        [Required]
+        [Column("email", TypeName = "VARCHAR")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Column("password", TypeName = "VARCHAR")]
+        public string Password { get; set; }
+
+        [Column("points", TypeName = "INT")]
+        public int Points { get; set; }
+
+        [ForeignKey("rol_id")]
+        public Role Rol_Id { get; set; }
+
+        public List<Transaction> Transactions { get; set; }
+
+        public Account Account { get; set; }
+
+    }
+
+}
