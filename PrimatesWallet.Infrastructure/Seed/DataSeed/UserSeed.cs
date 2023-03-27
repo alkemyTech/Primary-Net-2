a7e05a2ea296add7188010515bc739b847572df3
samueltribulo <samueltribulo@hotmail.com>
2023-03-27 16:43:54 -0300

    Model seed User

--- PrimatesWallet.Infrastructure/Seed/DataSeed/UserSeed.cs
+++ PrimatesWallet.Infrastructure/Seed/DataSeed/UserSeed.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PrimatesWallet.Core.Enums;
+using PrimatesWallet.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,59 @@ using System.Threading.Tasks;
 
 namespace PrimatesWallet.Infrastructure.Seed.DataSeed
 {
-    internal class UserSeed
+    public class UserSeed : SeedData<User>
     {
+        protected override IEnumerable<User> GetData()
+        {
+            return new List<User>()
+            {
+                new User()
+                {
+                    First_Name = "Samuel",
+                    Last_Name = "Tribulo",
+                    Email = "samueltribulo@hotmail.com",
+                    Password = "Password",
+                    Points = 0,
+                    Rol_Id = 1,
+                },
+                new User()
+                {
+                    First_Name = "Lionel",
+                    Last_Name = "Messi",
+                    Email = "liomessichampion@hotmail.com",
+                    Password = "liomessi",
+                    Points = 0,
+                    Rol_Id = 2,
+                },                
+                new User()
+                {
+                    First_Name = "Matias",
+                    Last_Name = "Cespedes",
+                    Email = "maticespedes@hotmail.com",
+                    Password = "maticespedes",
+                    Points = 0,
+                    Rol_Id = 1,
+                },
+                new User()
+                {
+                    First_Name = "Matias",
+                    Last_Name = "Boiero",
+                    Email = "matiboiero@hotmail.com",
+                    Password = "matiboiero",
+                    Points = 0,
+                    Rol_Id = 2,
+                },
+                new User()
+                {
+                    First_Name = "Luciano",
+                    Last_Name = "Escobar",
+                    Email = "luchoescobar@hotmail.com",
+                    Password = "luchoescobar",
+                    Points = 0,
+                    Rol_Id = 1,
+                }
+
+            };
+        }
     }
 }
