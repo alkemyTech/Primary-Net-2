dff7dc6c4d5b6fb3a35370749a3efc281b156bb9
mati <matias.cespedes@outlook.com>
2023-03-29 00:50:33 -0300

    PNT2-46: metodo en el servicio

--- PrimatesWallet.Api/Controllers/FixedTermDepositController.cs
+++ PrimatesWallet.Api/Controllers/FixedTermDepositController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using PrimatesWallet.Application.Exceptions;
 using PrimatesWallet.Application.Helpers;
 using PrimatesWallet.Application.Interfaces;
@@ -43,7 +44,8 @@ namespace PrimatesWallet.Api.Controllers
 
         }
 
-
+        [HttpGet]
+        [Authorize]
         public async Task<IActionResult> GetByUser()
         {
             try
