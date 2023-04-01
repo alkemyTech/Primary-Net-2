8f6958e64c227e9af789bd058142c469283beb30
Gonzalo <102692082+gnz6@users.noreply.github.com>
2023-04-01 15:52:52 -0300

    Agregar paginación a FixedTermDeposits

--- PrimatesWallet.Api/Controllers/FixedTermDepositController.cs
+++ PrimatesWallet.Api/Controllers/FixedTermDepositController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using PrimatesWallet.Api.Helpers;
+using PrimatesWallet.Application.DTOS;
 using PrimatesWallet.Application.Exceptions;
 using PrimatesWallet.Application.Helpers;
 using PrimatesWallet.Application.Interfaces;
+using PrimatesWallet.Application.Services;
 using PrimatesWallet.Core.Models;
 using System.Net;
 
@@ -65,5 +68,30 @@ namespace PrimatesWallet.Api.Controllers
                 return StatusCode(response.StatusCode, response);
             }
         }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetFixedTermDeposits([FromQuery] int page = 1, int pageSize = 10)
+        {
+            var allDeposits = await _fixedTermDeposit.GetDeposits(page, pageSize); //obtenemos solo los plazos fijos que necesitamos
+            var totalPages = await _fixedTermDeposit.TotalPageDeposits(pageSize); //obtenemos el total de paginas
+            string url = CurrentURL.Get(HttpContext.Request); //Clase estatica en helpers para obtener la url como string
+
+
+            var response = new BasePaginateResponse<IEnumerable<FixedTermDepositDetailDTO>>()
+            {
+                Message = ReplyMessage.MESSAGE_QUERY,
+                Result = allDeposits,
+                Page = page,
+                NextPage = (page < totalPages) ? $"{url}?page={page + 1}" : "None",
+                PreviousPage = (page == 1) ? "none" : $"{url}?page={page - 1}",
+                StatusCode = (int)HttpStatusCode.OK
+            };
+            return Ok(response);
+        }
+
+
+
+
     }
 }
--- PrimatesWallet.Infrastructure/repositories/FixedTermDepositRepository.cs
+++ PrimatesWallet.Infrastructure/repositories/FixedTermDepositRepository.cs
@@ -4,22 +4,36 @@ using PrimatesWallet.Core.Models;
 
 namespace PrimatesWallet.Infrastructure.repositories
 {
-    public class FixedTermDepositRepository : GenericRepository<FixedTermDeposit> ,IFixedTermDepositRepository
+    public class FixedTermDepositRepository : GenericRepository<FixedTermDeposit>, IFixedTermDepositRepository
     {
 
-        private readonly ApplicationDbContext _dbContext;
         public FixedTermDepositRepository(ApplicationDbContext context) : base(context)
         {
-            this._dbContext = context;
+
         }
 
 
-        public async Task<FixedTermDeposit> GetFixedTermByIdAndUserId(int  userId , int fixedId)
+        public async Task<FixedTermDeposit> GetFixedTermByIdAndUserId(int userId, int fixedId)
         {
             var accountDeposits = await _dbContext.Accounts.Where(x => x.UserId == userId).Include(x => x.FixedTermDeposit).FirstOrDefaultAsync();
             var fixedTermDeposit = accountDeposits.FixedTermDeposit.FirstOrDefault(x => x.Id == fixedId);
-            return fixedTermDeposit;  
+            return fixedTermDeposit;
 
+        }
+
+        public async Task<IEnumerable<FixedTermDeposit>> GetAll(int page, int pageSize)
+        {
+            //recuperamos en base de datos solo lo que necesitamos
+            return await base._dbContext.FixedTermDeposits
+                .Skip((page - 1) * pageSize) //saltamos lo anterior
+                .Take(pageSize) //tomamos los 10 que necesitamos
+                .ToListAsync();
+        }
+
+        public async Task<int> GetCount()
+        {
+            //la cuenta se hace en base de datos para eficiencia
+            return await base._dbContext.FixedTermDeposits.CountAsync();
         }
     }
 }
