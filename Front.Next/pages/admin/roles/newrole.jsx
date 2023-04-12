27e958ccbf15e8fe570224e22d5e0b3c79b9198c
Gonzalo <102692082+gnz6@users.noreply.github.com>
2023-04-12 12:19:11 -0300

    Page Creación de Roles

---
+++ Front.Next/pages/admin/roles/newrole.jsx
@@ -0,0 +1,141 @@
+import { Layout } from "@/Layouts/Layout";
+import { MyTextInput } from "@/components/MyTextInput"
+import { headers } from "@/next.config";
+import { Grid, Button, Link, Typography } from "@mui/material"
+import axios from "axios";
+import { Formik } from "formik"
+import { getSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import Swal from "sweetalert2";
+import * as Yup from "yup";
+
+
+const CreateRole = () => {
+    const router = useRouter();
+
+    const onSubmit = async (e, values) => {
+        e.preventDefault();
+        try {
+            const session = await getSession()
+            const { data } = await axios.post("https://localhost:7149/api/Role", values, {
+                headers: {
+                    'Authorization': `Bearer ${session.user?.token}`,
+                    "Content-Type": "application/json"
+                }
+            })
+            Swal.fire(
+                `${data}`,
+                ``,
+                'success'
+            )
+            router.back()
+        }
+        catch (error) {
+            Swal.fire(
+                `${error.message}`,
+                ``,
+                'error'
+            )
+        }
+
+    }
+
+
+    return (
+        <Layout title={"Create Role"}>
+
+            <Typography variant="h4">
+                Create Role
+            </Typography>
+            <Grid
+                container
+                display={"flex"}
+                flexDirection={"column"}
+                fullWidth
+                alignItems={"center"}
+                justifyContent={"center"}
+                sx={{ p: 4, borderRadius: 5, backgroundColor: "white", mt: 4 }}
+            >
+                <Grid
+                    container
+                    width={"50%"}
+                >
+
+
+
+                    <Formik
+
+                        initialValues={{ name: "", description: "" }}
+                        onSubmit={onSubmit}
+                        validationSchema={Yup.object({
+                            name: Yup.string().max(7, "Maximun length is 7 characters long"),
+                            description: Yup.string().min(10, "Please add a bried description")
+                        })}
+                    >
+                        {({
+                            values,
+                            errors,
+                            handleChange,
+                            handleBlur
+                        }) => (
+                            <form onSubmit={(e) => onSubmit(e, values)}>
+                                <Grid container>
+                                    <MyTextInput label={"Name"}
+                                        type={"text"}
+                                        placeholder={"New role name..."}
+                                        name={"name"}
+                                        value={values.name}
+                                        onBlur={handleBlur}
+                                        onChange={handleChange}
+                                        errors={errors.name}
+                                    />
+
+                                    <MyTextInput label={"Description"}
+                                        type={"text"}
+                                        placeholder={"Please add a description"}
+                                        name={"description"}
+                                        value={values.description}
+                                        onBlur={handleBlur}
+                                        onChange={handleChange}
+                                        errors={errors.description}
+                                        multiline
+                                        rows={4}
+                                    />
+
+                                </Grid>
+
+                                <Grid container fullWidth justifyContent={"center"} alignItems={"center"}>
+
+                                    <Grid item xs={12} sm={6}>
+                                        <Button
+
+                                            type="submit"
+                                            variant="contained"
+                                            fullWidth
+                                            sx={{
+                                                backgroundColor: "tertiary.main",
+                                                padding: 1,
+                                                mt: 2
+                                            }}
+                                        >
+                                            Create Role
+                                        </Button>
+                                    </Grid>
+                                </Grid>
+
+
+
+                            </form>
+                        )}
+
+
+                    </Formik>
+                </Grid>
+            </Grid>
+
+        </Layout>
+
+    )
+}
+
+export default CreateRole
