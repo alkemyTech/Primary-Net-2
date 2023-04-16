a237cfdf7234b65ce13e50f57c3cbd3cc64084a1
Gonzalo <102692082+gnz6@users.noreply.github.com>
2023-04-16 14:08:34 -0300

    cambios para el build en el front

--- Back.NET/PrimatesWallet.Api/Program.cs
+++ Back.NET/PrimatesWallet.Api/Program.cs
@@ -12,15 +12,6 @@ using PrimatesWallet.Application.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.AddCors(options =>
-//{
-//    options.AddDefaultPolicy(builder =>
-//    {
-//        builder.WithOrigins("http://localhost:3000")
-//               .AllowAnyHeader()
-//               .AllowAnyMethod();
-//    });
-//});
 
 {
     builder.Services.AddCors(options =>
